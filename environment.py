848e2e0e488b5c76b1d7226b962f93e8dd338b04
rahularya <rahularya50@gmail.com>
2018-10-18 15:26:56 -0700

    Implemented various Scheme primitives.

---
+++ environment.py
@@ -0,0 +1,23 @@
+from datamodel import Symbol, Nil, SingletonTrue, SingletonFalse
+from evaluate_apply import Frame
+
+def make_frame_decorator(defdict):
+    def global_builtin(name):
+        def decorator(cls):
+            cls.__repr__ = lambda self: f"#[{name}]"
+            defdict[Symbol(name)] = cls()
+            return cls
+        return decorator
+    return global_builtin
+
+defdict = {}
+global_attr = make_frame_decorator(defdict)
+
+def build_global_frame():
+    frame = Frame()
+    for k, v in defdict.items():
+        frame.assign(k, v)
+    frame.assign(Symbol("nil"), Nil)
+    frame.assign(Symbol("#t"), SingletonTrue)
+    frame.assign(Symbol("#f"), SingletonFalse)
+    return frame
---
+++ primitives.py
@@ -0,0 +1,32 @@
+from typing import List
+
+from gui import Holder
+
+from datamodel import Expression
+from evaluate_apply import Callable, Frame, evaluate_all
+from helper import verify_exact_callable_length
+
+
+class BuiltIn(Callable):
+    def execute(self, operands: List[Expression], frame: Frame, gui_holder: Holder) -> Expression:
+        operands = evaluate_all(operands, frame, gui_holder.expression.children[1:])
+        gui_holder.apply()
+        return self.execute_evaluated(operands, frame)
+
+    def execute_evaluated(self, operands: List[Expression], frame: Frame) -> Expression:
+        raise NotImplementedError()
+
+class SingleOperandPrimitive(BuiltIn):
+    def execute_evaluated(self, operands: List[Expression], frame: Frame) -> Expression:
+        verify_exact_callable_length(self, 1, len(operands))
+        operand = operands[0]
+        return self.execute_simple(operand)
+
+    def execute_simple(self, operand: Expression) -> Expression:
+        raise NotImplementedError()
+
+
+def load_primitives():
+    __import__("arithmetic")
+    __import__("lists")
+    __import__("type_checking")
