fa5a86ebc8f06ef36abf14245fdb8c061a1f4725
rahularya <rahularya50@gmail.com>
2018-12-17 11:24:58 +0800

    Made all libraries offline, rather than relying on a CDN.

--- ok_copy/client/cli/common/messages.py
+++
@@ -1,9 +0,0 @@
-class Messages(dict):
-    """A subclass of dictionary that prints a warning when an existing is
-    overwritten.
-    """
-    def __setitem__(self, key, value):
-        if key in self:
-            print('Warning: Overwriting key {}. '
-                  'Old: {}; New: {}'.format(key, self[key], value))
-        super().__setitem__(key, value)
