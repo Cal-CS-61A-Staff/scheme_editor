bb8ca9fc481b2ad8edef9ac059c87eb85b633c46
rahularya <rahularya50@gmail.com>
2018-12-21 16:46:48 +0800

    Implemented promises + stream (env diagram doesn't update correctly, though!)

--- test.scm
+++ test.scm
@@ -1,6 +1,8 @@
-(define (tailcall x)
-  (if (> x 0)
-      (tailcall (- x 1))
-      (print "wow")))
+; (define (tailcall x)
+;   (if (> x 0)
+;       (tailcall (- x 1))
+;       (print "wow")))
 
-(tailcall 100) ; expect tailcall
+; (tailcall 100) ; expect tailcall
+
+(delay 5)
