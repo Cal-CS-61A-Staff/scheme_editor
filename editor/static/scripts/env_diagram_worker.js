9a3f12851b3b3c988c9a6e3c06e8a11d6ad14b05
rahularya <rahularya50@gmail.com>
2018-12-24 03:29:48 +0800

    Prototype box + pointer implementation completed.

--- editor/static/scripts/env_diagram_worker.js
+++ editor/static/scripts/env_diagram_worker.js
@@ -39,6 +39,9 @@ function display_env_pointers(environments, heap, container, i) {
 
     let curr_y = 10;
 
+    let cache = new Map();
+
+    let h = charHeight;
     for (let frame of environments) {
         let maxlen = 0;
         let k;
@@ -46,16 +49,24 @@ function display_env_pointers(environments, heap, container, i) {
             if (frame["bindings"][k][0] > i) {
                 break;
             }
-            let line = "   " + frame["bindings"][k][2][0] + ": \n";
+            let line = "   " + frame["bindings"][k][1][0] + ": \n";
             maxlen = Math.max(maxlen, line.length);
             container.text(line).font("family", "Monaco, monospace").font("size", 14).dx(35).dy(curr_y + charHeight * (k + 1));
-            display_elem(100, curr_y + charHeight * (k + 1), frame["bindings"][k][2], heap, container, true)
+            let depth = display_elem(
+                200,
+                curr_y + h,
+                frame["bindings"][k][2],
+                heap,
+                container,
+                0,
+                cache) + 1;
+            h += depth * (minWidth + 15);
         }
         if (k === 0) {
             continue;
         }
         let title = "Frame " + frame.name + ": " + frame.label + " [parent = " + frame.parent + "]\n";
-        container.text(title).font("family", "Monaco, monospace").font("size", 14).dx(25).dy(curr_y);
+        // container.text(title).font("family", "Monaco, monospace").font("size", 14).dx(25).dy(curr_y);
         maxlen = Math.max(maxlen, title.length);
         let rect = container.rect(maxlen * charWidth + 10, charHeight * (k + 1) + 10)
             .dx(15).dy(curr_y)
@@ -67,41 +78,116 @@ function display_env_pointers(environments, heap, container, i) {
     }
 }
 
-function calc_length(elem) {
+let minWidth = charWidth * 4 + 5;
+
+function calc_content_length(elem) {
     if (elem[0]) {
-        return charWidth * 1.5;
+        return minWidth;
     } else {
-        return charWidth * elem[1].length + 5;
+        return Math.max(minWidth, charWidth * elem[1].length + 10);
     }
 }
 
-function display_elem(x, y, id, all_data, container, at_end) {
+function straight_arrow(container, x1, y1, x2, y2) {
+    container.circle(5).dx(x1 - 5 / 2).dy(y1 - 5 / 2);
+    let arrow = container
+        .polygon('0,0 -10,5 -10,-5')
+        .dx(x2).dy(y2)
+        .rotate(180 / Math.PI * Math.atan2(y2 - y1, x2 - x1), x2, y2);
+    let length = Math.hypot(x2 - x1, y2 - y1);
+    container
+        .line(x1, y1, x2 + (x1 - x2) / length * 5, y2 + (y1 - y2) / length * 5)
+        .stroke({width: 2, color: "#000000"});
+}
+
+function curved_arrow(container, x1, y1, x2, y2) {
+    straight_arrow(container, x1, y1, x2, y2);
+}
+
+// function calc_positions(root, all_data) {
+//     let occupied = [];
+//     function is_occupied(x, row) {
+//         while (occupied.length <= row) {
+//             occupied.push([]);
+//         }
+//         for (let elem of occupied[row]) {
+//             if (elem[0] > x) {
+//                 break;
+//             }
+//             if (elem[1] > x) {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+//
+//     function _calc_positions(id, row, x) {
+//         if
+//     }
+//     return _calc_positions(root);
+// }
+
+function display_elem(x, y, id, all_data, container, depth, cache) {
     console.log(id);
     console.log(all_data);
+    console.log(depth);
     if (id[0]) {
-        console.log(all_data[id[1]]);
         // non atomic
-        let len = 5;
-        if (at_end) {
-            container.line(x, y + charHeight / 2 + 5 / 2, x + 20, y + charHeight / 2 + 5 / 2)
-                .stroke({width: 3, color: "#c8c8c8"}).back();
-            x += 20;
+        let x1 = x + minWidth / 2;
+        let y1 = y + minWidth / 2;
+        if (cache.has(id[1])) {
+            curved_arrow(container, x1, y1, ...cache.get(id[1]));
+            return 0;
+        }
+        let x2, y2;
+        if (depth === 0) {
+            cache.set(id[1], [x, y]);
+            x2 = x + minWidth + 15;
+            y2 = y + minWidth / 2;
+            x = x2;
         } else {
-            console.log("atom")
+            x2 = x + minWidth / 2;
+            y2 = y + (minWidth + 15) * depth;
+            y = y2;
         }
-        let i = 0;
+        if (all_data[id[1]].length > 1) {
+            cache.set(id[1], [x2, y2]);
+        } else {
+            cache.set(id[1], [x, y + minWidth / 2]);
+        }
+        straight_arrow(container, x1, y1, ...cache.get(id[1]));
+        let pos = 0;
+        let lens = [];
         for (let elem of all_data[id[1]]) {
-            i += 1;
-            display_elem(x + len, y, elem, all_data, container, i === all_data[id[1]].length);
-            len += calc_length(elem);
+            lens.push(pos);
+            pos += calc_content_length(elem);
         }
-        container.rect(len, charHeight + 5)
-            .dx(x).dy(y)
-            .stroke({color: "#000000", width: 2})
-            .fill({color: "#FFFFFF"})
-            .radius(10).back();
+        let new_depth = 0;
+        for (let i = all_data[id[1]].length - 1; i >= 0; --i) {
+            if (i !== 0) {
+                container.line(x + lens[i], y, x + lens[i], y + minWidth).stroke({color: "#000000", width: 2});
+            }
+            let elem = all_data[id[1]][i];
+            if (i !== all_data[id[1]].length - 1 && elem[0]) {
+                new_depth += 1;
+            }
+            new_depth += display_elem(x + lens[i], y, elem, all_data, container, new_depth, cache);
+        }
+        if (all_data[id[1]].length > 1) {
+            container.rect(pos, minWidth)
+                .dx(x).dy(y)
+                .stroke({color: "#000000", width: 2})
+                .fill({color: "#FFFFFF"}).back();
+        }
+        // container.text(new_depth.toString(10)).dx(x).dy(y);
+        return new_depth;
     } else {
         // atomic
-        container.text(id[1]).font("family", "Monaco, monospace").font("size", 14).dx(x).dy(y);
+        let width = calc_content_length(id);
+        container.text(id[1])
+            .font("family", "Monaco, monospace").font("size", 14)
+            .cx(x + width / 2)
+            .cy(y + minWidth / 2);
+        return 0;
     }
 }
