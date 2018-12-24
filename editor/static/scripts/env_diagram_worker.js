4eaa3bfb3101d828ec1550543598494decbba5ae
rahularya <rahularya50@gmail.com>
2018-12-24 18:33:50 +0800

    Fully integrated box + pointer diagrams into the env_diagram viewer, made some changes that need to be propagated to the original env_worker.

--- editor/static/scripts/env_diagram_worker.js
+++ editor/static/scripts/env_diagram_worker.js
@@ -37,44 +37,81 @@ function display_env(environments, container, i) {
 function display_env_pointers(environments, heap, container, i) {
     container.clear();
 
-    let curr_y = 10;
-
     let cache = new Map();
 
     let h = charHeight;
+
+    let frame_data = [];
+    let maxlen = 0;
+
     for (let frame of environments) {
-        let maxlen = 0;
+        let curr = [["", false]];
         let k;
         for (k = 0; k !== frame["bindings"].length; ++k) {
             if (frame["bindings"][k][0] > i) {
                 break;
             }
-            let line = "   " + frame["bindings"][k][1][0] + ": \n";
+            if (frame["bindings"][k][1][0] === "Return Value" && frame["bindings"][k][0] < i) {
+                k = 0;
+                break;
+            }
+            let line = "   " + frame["bindings"][k][1][0];
+            let data = frame["bindings"][k][2];
+            if (data[0]) {
+                // non-atomic
+            } else {
+                line += ": " + data[1];
+                data = false;
+            }
+            curr.push([line, data]);
             maxlen = Math.max(maxlen, line.length);
-            container.text(line).font("family", "Monaco, monospace").font("size", 14).dx(35).dy(curr_y + charHeight * (k + 1));
-            let depth = display_elem(
-                200,
-                h,
-                frame["bindings"][k][2],
-                heap,
-                container,
-                0,
-                cache) + 1;
-            h += depth * (minWidth + 15);
         }
         if (k === 0) {
             continue;
         }
-        let title = "Frame " + frame.name + ": " + frame.label + " [parent = " + frame.parent + "]\n";
-        // container.text(title).font("family", "Monaco, monospace").font("size", 14).dx(25).dy(curr_y);
-        maxlen = Math.max(maxlen, title.length);
-        let rect = container.rect(maxlen * charWidth + 10, charHeight * (k + 1) + 10)
+        let title = frame.name + ": " + frame.label;
+        if (frame.parent !== "Global" && frame.name !== "Global") {
+            title += " [parent = " + frame.parent + "]";
+        }
+        title += "\n";
+        curr[0] = [title, false];
+        frame_data.push(curr);
+    }
+
+    maxlen += 5;
+
+    let curr_y = 10;
+
+    for (let frame of frame_data) {
+        for (let k = 1; k < frame.length; ++k) {
+            container.text(frame[k][0]).font("family", "Monaco, monospace").font("size", 14).dx(35).dy(curr_y + charHeight * k);
+            if (frame[k][1]) {
+                let is_box = (heap[frame[k][1][1]].length > 1);
+                let depth = display_elem(
+                    maxlen * charWidth + 45,
+                    h + (is_box ? charHeight / 2 : 0),
+                    frame[k][1],
+                    heap,
+                    container,
+                    0,
+                    cache,
+                    maxlen * charWidth,
+                    curr_y + charHeight * k + charHeight * 3 / 4) + 1;
+                if (is_box) {
+                    h += depth * (minWidth + 15);
+                } else {
+                    h += charHeight;
+                }
+            }
+        }
+        container.text(frame[0][0]).font("family", "Monaco, monospace").font("size", 14).dx(25).dy(curr_y);
+        let rect = container.rect(maxlen * charWidth + 10, charHeight * frame.length + 10)
             .dx(15).dy(curr_y)
             .stroke({color: "#000000", width: 2})
             .fill({color: "#FFFFFF"})
             .radius(10).back();
 
-        curr_y += charHeight * (k + 1) + 20;
+        curr_y += charHeight * frame.length + 20;
     }
 }
 
@@ -104,37 +141,13 @@ function curved_arrow(container, x1, y1, x2, y2) {
     straight_arrow(container, x1, y1, x2, y2);
 }
 
-// function calc_positions(root, all_data) {
-//     let occupied = [];
-//     function is_occupied(x, row) {
-//         while (occupied.length <= row) {
-//             occupied.push([]);
-//         }
-//         for (let elem of occupied[row]) {
-//             if (elem[0] > x) {
-//                 break;
-//             }
-//             if (elem[1] > x) {
-//                 return true;
-//             }
-//         }
-//         return false;
-//     }
-//
-//     function _calc_positions(id, row, x) {
-//         if
-//     }
-//     return _calc_positions(root);
-// }
-
-function display_elem(x, y, id, all_data, container, depth, cache) {
-    console.log(id);
-    console.log(all_data);
-    console.log(depth);
+function display_elem(x, y, id, all_data, container, depth, cache, x1=false, y1=false) {
     if (id[0]) {
         // non atomic
-        let x1 = x + minWidth / 2;
-        let y1 = y + minWidth / 2;
+        if (!x1) {
+            x1 = x + minWidth / 2;
+            y1 = y + minWidth / 2;
+        }
         if (cache.has(id[1])) {
             curved_arrow(container, x1, y1, ...cache.get(id[1]));
             return 0;
--- questions.scm
+++ questions.scm
@@ -1,6 +1,6 @@
 (define cat `((1 ,cons) ,cons 3))
 
-(define dog (cons (cons (car '(1 2 3)) (list 2 3 4))
+(define dog (cons-stream (cons (car '(1 2 3)) (list 2 3 4))
 (cons 2 3)))
 
 (define mouse ''())
