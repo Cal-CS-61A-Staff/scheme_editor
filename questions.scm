0de51261759367e242d2709a63e6e59dbfcffc43
rahularya <rahularya50@gmail.com>
2019-01-05 19:07:48 +0800

    Implemented correct stream diagramming.

--- questions.scm
+++ questions.scm
@@ -1,38 +1,7 @@
-; (define (reverse lst)
-;   (cond
-;   ((null? lst)
-;     nil)
-;   (else
-;     (append (reverse (cdr lst))
-;             (list (car lst))))))
-; ;;; bad
-; (define (tail-reverse lst)
-;   (define (helper lst curr)
-;     (if (null? lst)
-;         curr
-;         (helper (cdr lst)
-;                 (cons (car lst) curr))))
-;   (helper lst '()))
-; (define (f x)
-;   (if (< (g x) 3)
-;       (h x 2)))
-; (define (prime? x)
-;   (if (<= x 1)
-;       #f
-;       (null? (filter (lambda (y)
-;                              (= 0 (remainder x y)))
-;                      (range 2 x)))))
-(define (add n)
-  (if (= n 0)
-      'done
-      (add (- n 1))))
+(define a (cons-stream 1 (delay 2)))
 
-; (add 1)
+(define b (cdr a))
 
-(define (f) 5)
+(print (cdr-stream a))
 
-(define (tailcall n)
-  (if (= n 0)
-      0
-      (tailcall (- n 1))))
- 
+(define a 5)
