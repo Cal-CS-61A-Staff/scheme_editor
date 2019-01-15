f3489a3a901b0f3d528e4fa5edcfa404d16f1705
rahularya <rahularya50@gmail.com>
2019-01-14 23:46:12 -0500

    Fixed bug causing uneven distribution in box + pointer visualizer.

--- questions.scm
+++ questions.scm
@@ -1,7 +1,15 @@
-(define a (cons-stream 1 (delay 2)))
+(define (reverse lst)
+  (if (null? lst)
+      nil
+      (cons (reverse (cdr lst))
+            (car lst))))
 
-(define b (cdr a))
+(define (iota n)
+  (define (loop acc k)
+    (if (= k n)
+        (reverse acc)
+        (loop (append (list k) acc)
+              (+ 1 k))))
+  (loop nil 0))
 
-(print (cdr-stream a))
-
-(define a 5)
+(iota 5)
