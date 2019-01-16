480b0284a5cc7fcbb70b0ae154a39e1c5764c013
rahularya <rahularya50@gmail.com>
2019-01-15 22:35:36 -0500

    Began implementing a compact tree visualizer.

--- editor/static/scripts/substitution_tree.js
+++ editor/static/scripts/substitution_tree.js
@@ -1,10 +1,11 @@
-import * as state_handler from "./state_handler";
 import * as substitution_tree_worker from "./substitution_tree_worker"
-import {states} from "./state_handler";
-import {notify_open} from "./layout";
-import {make} from "./event_handler";
+import {
+    make
+} from "./event_handler";
 
-export { register };
+export {
+    register
+};
 
 function register(myLayout) {
     myLayout.registerComponent('substitution_tree', function (container, componentState) {
@@ -51,18 +52,18 @@ function register(myLayout) {
                 <div class="tree">
                     <svg></svg>
                 </div>
+                <div class="flag"></div>
             </div>
         `);
 
         make(container, "substitution_tree", componentState.id);
 
         let rawSVG = container.getElement().find(".tree > svg").get(0);
-        // svgPanZoom(rawSVG, {fit: false, zoomEnabled: true, center: false, controlIconsEnabled: true});
         let svg = SVG.adopt(rawSVG).size(container.width, container.height);
         console.log("adopted");
         let ready = false;
 
-        container.getElement().find(".tree").on("update", function (e) {
+        container.getElement().find(".flag").on("update", () => {
             let zoom = svgPanZoom(rawSVG).getZoom();
             let pan = svgPanZoom(rawSVG).getPan();
             svgPanZoom(rawSVG).destroy();
@@ -72,14 +73,23 @@ function register(myLayout) {
                 ready = true;
                 console.log("SKIP");
             }
-            substitution_tree_worker.display_tree(componentState.id, svg);
-            svgPanZoom(rawSVG, {fit: false, zoomEnabled: true, center: false, controlIconsEnabled: true});
-            if (isNaN(zoom)) {
-                svgPanZoom(rawSVG).reset();
-            } else {
-                svgPanZoom(rawSVG).zoom(zoom);
-                svgPanZoom(rawSVG).pan(pan);
-            }
+            console.log("starting");
+            substitution_tree_worker.display_tree(componentState.id, svg,
+                function () {
+                    svgPanZoom(rawSVG, {
+                        fit: false,
+                        zoomEnabled: true,
+                        center: false,
+                        controlIconsEnabled: true
+                    });
+                    console.log("cat");
+                    if (isNaN(zoom)) {
+                        svgPanZoom(rawSVG).reset();
+                    } else {
+                        svgPanZoom(rawSVG).zoom(zoom);
+                        svgPanZoom(rawSVG).pan(pan);
+                    }
+                });
         });
 
         container.getElement().find(".tree").on("reset", function () {
@@ -93,7 +103,12 @@ function register(myLayout) {
             svgPanZoom(rawSVG).destroy();
             svg.size(container.width, container.height);
             console.log("ready!");
-            svgPanZoom(rawSVG, {fit: false, zoomEnabled: true, center: false, controlIconsEnabled: true});
+            svgPanZoom(rawSVG, {
+                fit: false,
+                zoomEnabled: true,
+                center: false,
+                controlIconsEnabled: true
+            });
             if (isNaN(zoom)) {
                 svgPanZoom(rawSVG).reset();
             } else {
