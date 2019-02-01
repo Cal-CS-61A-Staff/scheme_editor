0a6f9eccaf787293685bb5cea14c8e53b8266bd5
Rahul Arya <rahularya@berkeley.edu>
2019-02-01 00:38:35 -0800

    Fixed inconsistent CSS across browsers.

--- editor/__main__.py
+++ editor/__main__.py
@@ -4,18 +4,21 @@ import os
 import local_server
 
 parser = argparse.ArgumentParser(description="Rahul's Scheme IDE!")
-parser.add_argument("file",
+parser.add_argument("-f", "--file",
                     type=argparse.FileType('r+'),
                     help="Scheme file to open")
-parser.add_argument("-d", "--debug",
-                    help="Enable debug mode",
+parser.add_argument("-l", "--logging",
+                    help="Print log statements",
                     action="store_true")
-parser.add_argument("-s", "--scm",
-                    help="start SCM searcher",
-                    action="store_true")
-parser.add_argument("-t", "--terminal")
+parser.add_argument("-p", "--port",
+                    type=int,
+                    default=31415,
+                    help="Choose the port to access the editor")
 args = parser.parse_args()
 
-file_name = os.path.basename(args.file.name)
-args.file.close()
-local_server.start(file_name)
+if args.file is not None:
+    file_name = os.path.basename(args.file.name)
+    args.file.close()
+else:
+    file_name = "hw10.scm"
+local_server.start(file_name, args.port)
--- hw10.scm
+++ hw10.scm
@@ -1,3 +1,24 @@
 (define (factorial x)
-    (if (= x 1) 1
-        (* x (factorial (- x 1)))))
+  (define (helper x curr)
+    (if (= x 1)
+        curr
+        (helper (- fx 1)
+                (* x curr))))
+  (helper x 1))
+
+(define (accumulate-tail op start num term)
+  (if (= num 0)
+      start
+      (accumulate-tail op
+                       (op start (term num))
+                       (- num 1)
+                       term)))
+
+(define (partial-sums stream)
+  (define (helper curr s)
+    (if (null? s)
+        nil
+        (cons-stream (+ curr (car s))
+                     (helper (+ curr (car s))
+                             (cdr-stream s)))))
+  (helper 0 stream))
