5d085ccf6946f3a023425dcdd3cea403dac5eed0
Rahul Arya <rahularya@berkeley.edu>
2019-02-12 10:16:40 -0800

    Prevents weird highlighting behavior in console, fixes #11

--- sorter.scm
+++ sorter.scm
@@ -1,14 +1,1 @@
-(define (sort lst)
-  (if (null? lst)
-      nil
-      (let ((sortedRest (sort (cdr lst))))
-        (define (insert val sortedRest)
-          (cond
-           ((null? sortedRest)
-            (list val))
-           ((> val (car sortedRest))
-            (cons (car sortedRest)
-                  (insert val (cdr sortedRest))))
-           (else
-            (cons val sortedRest))))
-        (insert (car lst) sortedRest))))
+(define-macro (dotwice expr) `(begin ,expr ,expr))
