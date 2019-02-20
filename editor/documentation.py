7cbe435a39b4737fc71d904bb64e767fadcbc0b0
Rahul Arya <rahularya@berkeley.edu>
2019-02-20 14:41:35 -0800

    Implemented functioning searchable documentation.

--- editor/documentation.py
+++ editor/documentation.py
@@ -1,10 +1,22 @@
+import re
+
 from libraries import mistune
 
 
 def search(query):
     with open("editor/scheme_documentation.md") as f:
+        query = query.strip().lower()
+
         contents = str(f.read())
-        return [mistune.markdown(contents)]
+        contents = re.sub(r"<a class='builtin-header' id='.*?'>\*\*(.*?)\*\*</a>", r"### **\1**", contents)
 
+        divider = "##"
+        elements = [divider + elem for elem in contents.split(divider)]
+        elements = [elem for elem in elements if "\n" in elem.strip()]
+        relevant_elems = []
+        for elem in elements:
+            if query in elem.lower():
+                relevant_elems.append(elem)
 
-search("cond")
+        relevant_elems.sort(key=lambda x: 20 * x.split("\n")[0].count(query) / len(x.split("\n")[0]) + x.count(query) / len(x), reverse=True)
+        return [mistune.markdown(elem) for elem in relevant_elems]
