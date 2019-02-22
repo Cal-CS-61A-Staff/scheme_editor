cc4242456135779ae4dceec099ecd067568301f4
Rahul Arya <rahularya@berkeley.edu>
2019-02-22 02:08:20 -0800

    Added a set of tests for the interpreter.
    Currently, they only test the output, not any of the logging.

--- editor_tests/decode_scm_tests.py
+++ editor_tests/decode_scm_tests.py
@@ -1,14 +1,65 @@
+from scheme_runner import Query, SchemeTestCase
+
+
+def balanced(code):
+    return sum(x.count("(") - x.count(")") for x in code) == 0
+
+
 def decode(filename: str):
     if not filename.endswith(".scm"):
         filename += ".scm"
     with open(filename) as f:
-        contents = str(f)
-        tests = []
+        contents = str(f.read())
+        groups = {}
+        curr_case = None
         for line in contents.split("\n"):
-            if line.startswith(";"):
+            line = line.strip()
+            # if not line:
+            #     if not curr_case or not curr_case.queries or not curr_case.queries[-1].code:
+            #         continue
+            #     most_recent_code = curr_case.queries[-1].code[-1]
+            #     if most_recent_code.count("(") != most_recent_code.count(")"):
+            #         # heuristic check for finished expressions
+            #         continue
+            #     curr_case.queries.append(Query(code=[], expected={}))
+            # el
+
+            if not line:
+                continue
+            elif line.startswith(";;; group>"):
+                curr_case = SchemeTestCase([Query(code=[], expected={})])
+                groups[line.split("group>", 1)[1]] = curr_case
+            elif line.startswith(";"):
                 if not line.startswith("; expect"):
                     continue  # useless comments
-                tests[-1].append(line.split("; expect")[0])
-            elif line:
-                tests.append([line])
-        print(tests)
+                expect_str = line.split("; expect")[1]
+                curr_case.queries[-1].expected["out"] = \
+                    [curr_case.queries[-1].expected.get("out", [""])[0]
+                     + "".join(x.strip() + "\n" for x in expect_str.split(";"))]
+            elif not balanced(curr_case.queries[-1].code):
+                curr_case.queries[-1].code.append(line)
+            else:
+                curr_case.queries.append(Query([line], {}))
+
+    print(groups)
+
+    for group in groups:
+        filename = f"scm_tests/case_{group.strip().lower().replace(' ', '-')}.py"
+
+        with open(filename, "w") as f:
+            f.write("")
+
+        def write(x):
+            print(x)
+            with open(filename, "a") as f:
+                f.write(x + "\n")
+
+        write("from scheme_runner import SchemeTestCase, Query")
+        write("cases = [")
+        write(repr(groups[group]))
+        write("]")
+
+
+if __name__ == '__main__':
+    print("# stdout is a viable test file")
+    decode(input("# input filename: "))
--- editor_tests/scm_tests/case_arithmetic.py
+++ editor_tests/scm_tests/case_arithmetic.py
@@ -1,13 +1,4 @@
 from scheme_runner import SchemeTestCase, Query
-
-
-def out(*expected):
-    return {"out": ["\n".join(expected) + "\n"]}
-
-
 cases = [
-    SchemeTestCase([Query("10", out("10"))]),
-    SchemeTestCase([Query("(+ 137 349)", out("486"))]),
-    SchemeTestCase([Query("(- 1000 334)", out("666"))]),
-
+SchemeTestCase([Query(code=[], expected={}), Query(code=['10'], expected={'out': ['10\n']}), Query(code=['(+ 137 349)'], expected={'out': ['486\n']}), Query(code=['(- 1000 334)'], expected={'out': ['666\n']}), Query(code=['(* 5 99)'], expected={'out': ['495\n']}), Query(code=['(/ 10 5)'], expected={'out': ['2\n']}), Query(code=['(+ 2.7 10)'], expected={'out': ['12.7\n']}), Query(code=['(+ 21 35 12 7)'], expected={'out': ['75\n']}), Query(code=['(* 25 4 12)'], expected={'out': ['1200\n']}), Query(code=['(+ (* 3 5) (- 10 6))'], expected={'out': ['19\n']}), Query(code=['(+ (* 3 (+ (* 2 4) (+ 3 5))) (+ (- 10 7) 6))'], expected={'out': ['57\n']}), Query(code=['(+ (* 3', '(+ (* 2 4)', '(+ 3 5)))', '(+ (- 10 7)', '6))'], expected={'out': ['57\n']})])
 ]
