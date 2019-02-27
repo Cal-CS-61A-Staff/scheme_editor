02674c92b38c87e351e87fb741b210feb377e662
Rahul Arya <rahularya@berkeley.edu>
2019-02-26 17:39:30 -0800

    *Actually* undone modifications to demo .scm files.

--- hw10.scm
+++ hw10.scm
@@ -1,24 +1,24 @@
-(define (factorial x)	 (define-macro (f x) x)
-  (define (helper x curr)	(f (+ 1 2)) 
-    (if (= x 1)	
-        curr	
-        (helper (- fx 1)	
-                (* x curr))))	
-  (helper x 1))	
+(define (factorial x)
+  (define (helper x curr)
+    (if (= x 1)
+        curr
+        (helper (- fx 1)
+                (* x curr))))
+  (helper x 1))
 
- (define (accumulate-tail op start num term)s	
-  (if (= num 0)	
-      start	
-      (accumulate-tail op	
-                       (op start (term num))	
-                       (- num 1)	
-                       term)))	
+(define (accumulate-tail op start num term)s
+  (if (= num 0)
+      start
+      (accumulate-tail op
+                       (op start (term num))
+                       (- num 1)
+                       term)))
 
- (define (partial-sums stream)	
-  (define (helper curr s)	
-    (if (null? s)	
-        nil	
-        (cons-stream (+ curr (car s))	
-                     (helper (+ curr (car s))	
-                             (cdr-stream s)))))	
+(define (partial-sums stream)
+  (define (helper curr s)
+    (if (null? s)
+        nil
+        (cons-stream (+ curr (car s))
+                     (helper (+ curr (car s))
+                             (cdr-stream s)))))
   (helper 0 stream))
