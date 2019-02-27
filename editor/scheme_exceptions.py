26a19d59e38784a83a8211b6e2a68604b4a6fa8c
Rahul Arya <rahularya@berkeley.edu>
2019-02-27 15:47:04 -0800

    PEP8 blah blah

--- editor/scheme_exceptions.py
+++ editor/scheme_exceptions.py
@@ -2,36 +2,47 @@ class SchemeError(Exception):
     def __repr__(self):
         return str(self)
 
+
 class TerminatedError(SchemeError):
     def __init__(self):
         super().__init__("Error: program manually terminated")
 
 
-class ParseError(SchemeError): pass
+class ParseError(SchemeError):
+    pass
 
 
-class SymbolLookupError(SchemeError): pass
+class SymbolLookupError(SchemeError):
+    pass
 
 
-class OperandDeduceError(SchemeError): pass
+class OperandDeduceError(SchemeError):
+    pass
 
 
-class CallableResolutionError(SchemeError): pass
+class CallableResolutionError(SchemeError):
+    pass
 
 
-class MathError(SchemeError): pass
+class MathError(SchemeError):
+    pass
 
 
-class ComparisonError(SchemeError): pass
+class ComparisonError(SchemeError):
+    pass
 
 
-class TypeMismatchError(SchemeError): pass
+class TypeMismatchError(SchemeError):
+    pass
 
 
-class IrreversibleOperationError(SchemeError): pass
+class IrreversibleOperationError(SchemeError):
+    pass
 
 
-class FormatError(SchemeError): pass
+class FormatError(SchemeError):
+    pass
 
 
-class LoadError(SchemeError): pass
+class LoadError(SchemeError):
+    pass
