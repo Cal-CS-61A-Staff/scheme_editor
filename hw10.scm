b68a484b3bf1462f0483416ffc588d58e367ba2c
Rahul Arya <rahularya@berkeley.edu>
2019-02-27 20:57:37 -0800

    Fixed.

--- hw10.scm
+++ hw10.scm
@@ -1,25 +1,4 @@
-(define (factorial x)
-  (define (helper x curr)
-    (if (= x 1)
-        curr
-        (helper (- fx 1)
-                (* x curr))))
-  (helper x 1))
+(define (loop n)
+  (if (zero? n) (/ 1 0) (loop (- n 1))))
 
-(define (accumulate-tail op start num term)
-  s
-  (if (= num 0)
-      start
-      (accumulate-tail op
-                       (op start (term num))
-                       (- num 1)
-                       term)))
-
-(define (partial-sums stream)
-  (define (helper curr s)
-    (if (null? s)
-        nil
-        (cons-stream (+ curr (car s))
-                     (helper (+ curr (car s))
-                             (cdr-stream s)))))
-  (helper 0 stream))
+(loop 300)
