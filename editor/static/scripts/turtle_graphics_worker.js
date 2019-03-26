1705584bdaff886495a3372e56599603d81443d3
Rahul Arya <rahularya@berkeley.edu>
2019-03-25 20:14:27 -0700

    Implemented turtle visualization.

--- editor/static/scripts/turtle_graphics_worker.js
+++ editor/static/scripts/turtle_graphics_worker.js
@@ -5,6 +5,12 @@ function draw(svg, rawSVG, data) {
     for (let move of data["path"]) {
         svg.path(move["seq"])
             .fill(move["fill"])
-            .stroke({ color: move["stroke"], width: 1, linecap: 'round', linejoin: 'round' });
+            .stroke({color: move["stroke"], width: 1, linecap: 'round', linejoin: 'round'});
+    }
+
+    if (data["showTurtle"]) {
+        svg.polygon('0,0 -10,5 -10,-5')
+            .dx(data["turtleX"]).dy(data["turtleY"])
+            .rotate(data["turtleRot"], data["turtleX"], data["turtleY"]);
     }
 }
