3ede4d20b94aaabb87822a70a4f2a34a6b012ca8
Rahul Arya <rahularya@berkeley.edu>
2019-04-05 19:20:18 -0700

    Handle comments on newlines better (#119)
    * Make the formatter treat comments on newlines a bit better.
    
    * Tweaked comment handling for HW08.
    
    * Removed print statement.
    
    * Changed behavior slightly.

--- editor/formatter.py
+++ editor/formatter.py
@@ -91,7 +91,7 @@ def log(message: str):
 
 def prettify_expr(expr: Formatted, remaining: int) -> Tuple[str, bool]:
     if isinstance(expr, FormatAtom):
-        if len(expr.comments) <= 1:
+        if len(expr.comments) <= 1 and expr.allow_inline:
             return verify(inline_format(expr) + make_comments(expr.comments, len(expr.value), False), remaining)
         else:
             return verify(make_comments(expr.comments, len(expr.value), True) + inline_format(expr), remaining)
@@ -101,7 +101,7 @@ def prettify_expr(expr: Formatted, remaining: int) -> Tuple[str, bool]:
         if verify(expr_str, remaining)[1]:
             out1 = expr_str + make_comments(expr.comments, len(expr_str), False)
             out2 = make_comments(expr.comments, len(expr_str), True) + expr_str
-            if len(expr.comments) <= 1 and verify(out1, remaining)[1]:
+            if len(expr.comments) <= 1 and expr.allow_inline and verify(out1, remaining)[1]:
                 return verify(out1, remaining)
             elif verify(out2, remaining)[1]:
                 return verify(out2, remaining)
@@ -126,7 +126,7 @@ def prettify_expr(expr: Formatted, remaining: int) -> Tuple[str, bool]:
             return out
         elif not expr.contents:
             # nil expr
-            if len(expr.comments) <= 1:
+            if len(expr.comments) <= 1 and expr.allow_inline:
                 return verify(expr.open_paren + expr.close_paren + make_comments(expr.comments, 2, False), remaining)
             else:
                 return verify(make_comments(expr.comments, 2, True) + expr.open_paren + expr.close_paren, remaining)
@@ -210,7 +210,7 @@ def prettify_expr(expr: Formatted, remaining: int) -> Tuple[str, bool]:
                                         + val_str + clause.close_paren
 
                                     ok = False
-                                    if len(clause.comments) > 1:
+                                    if len(clause.comments) > 1 or not clause.allow_inline:
                                         clause_str = make_comments(clause.comments, 0, True) + clause_str
                                         ok = verify(clause_str, remaining - 1)[1]
                                     if not ok:
@@ -258,8 +258,7 @@ def prettify_expr(expr: Formatted, remaining: int) -> Tuple[str, bool]:
                         out_str += "\n"
                     out_str += expr.close_paren
                     out = verify(make_comments(expr.comments, 0, True) + out_str, remaining)
-                    if out[1]:
-                        return out
+                    return out
             # but may have to go here anyway, if inlining takes up too much space
             return prettify_data(expr, remaining, False)
     else:
@@ -269,7 +268,7 @@ def prettify_expr(expr: Formatted, remaining: int) -> Tuple[str, bool]:
 
 def prettify_data(expr: Formatted, remaining: int, is_data: bool, force_multiline: bool = False) -> Tuple[str, bool]:
     if isinstance(expr, FormatAtom):
-        if len(expr.comments) <= 1:
+        if len(expr.comments) <= 1 and expr.allow_inline:
             return verify(inline_format(expr) + make_comments(expr.comments, len(expr.value), False), remaining)
         else:
             return verify(make_comments(expr.comments, len(expr.value), True) + inline_format(expr), remaining)
@@ -291,7 +290,7 @@ def prettify_data(expr: Formatted, remaining: int, is_data: bool, force_multilin
         if verify(expr_str, remaining)[1]:
             out1 = expr_str + make_comments(expr.comments, len(expr_str), False)
             out2 = make_comments(expr.comments, len(expr_str), True) + expr_str
-            if len(expr.comments) <= 1 and verify(out1, remaining)[1]:
+            if len(expr.comments) <= 1 and expr.allow_inline and verify(out1, remaining)[1]:
                 return verify(out1, remaining)
             else:
                 return verify(out2, remaining)
