1993398e372729f717ef016f6f343ccd358adca1
Rahul Arya <rahularya@berkeley.edu>
2019-04-10 15:38:06 -0700

    The disconnected modal will display on all ajax fails (#150)
    * The disconnected modal will display on all ajax fails, not just on the 5 second timer.
    
    * Also make sure you can't press ESC to exit a disconnected warning.

--- editor/static/scripts/main.js
+++ editor/static/scripts/main.js
@@ -5,7 +5,7 @@ import * as settings from "./settings";
 import * as documentation from "./documentation";
 import * as keyboard_shortcuts from "./keyboard_shortcuts";
 import {loadState} from "./state_handler";
-import {request_update} from "./event_handler";
+import {end_slow, request_update} from "./event_handler";
 
 $(window).on("load", async function () {
     await loadState();
@@ -16,6 +16,11 @@ $(window).on("load", async function () {
     settings.init();
     documentation.init();
     keyboard_shortcuts.init();
+
+    $(document).ajaxError(() => {
+        end_slow();
+        $("#disconnectedModal").modal("show");
+    });
 
     request_update();
 });
